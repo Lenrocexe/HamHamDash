6f567d68f21c7dd238f7cbf4b41e9df028d106e1
Robin Stroet <rre.stroet@gmail.com>
2010-04-16 12:01:13 +0200

    Updated diamond

--- src/hamhamdash/Diamond.java
+++ src/hamhamdash/Diamond.java
@@ -8,17 +8,30 @@ import jgame.*;
  */
 public class Diamond extends GObject
 {
+	public int diamondPoint = 3; // Points for diamond
+
 	public Diamond(String name, boolean unique, int x, int y, int cid, String sprite, Game game)
 	{
 		super(name, unique, x, y, cid, sprite, game);
 	}
 
+
 	@Override
 	public void move(){};
+
 
 	@Override
 	public void hit_bg(int tilecid){};
 
+//		pickupDiamond();
+
 	@Override
 	public void hit(JGObject obj){};
+
+	public void pickupDiamond()
+	{
+//		if (tile.contains("P"))
+
+		destroy();
+	}
 }
