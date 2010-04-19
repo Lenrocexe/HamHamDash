6232d1d7a555ee960490e135eb438d036081806a
Serhan Uygur <serhan.uygur@gmail.com>
2010-04-19 12:32:09 +0200

    Minor changes

--- src/hamhamdash/Game.java
+++ src/hamhamdash/Game.java
@@ -394,7 +394,6 @@ public class Game extends JGEngine
 	public void setFieldSize(int[] t)
 	{
 		setPFSize(t[0], t[1]);
-//		System.out.println(objLevels.getCurrentLevelSize()[0] + " " + objLevels.getCurrentLevelSize()[1]);
 	}
 
 	public Level getCurrentLevel()
