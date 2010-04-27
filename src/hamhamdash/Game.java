e59cec4329cd53dc016bdf60b080dbfefad0e2d7
Jelle Kralt <jelle@jellekralt.nl>
2010-04-27 11:53:19 +0200

    Turned off debug mode

--- src/hamhamdash/Game.java
+++ src/hamhamdash/Game.java
@@ -18,7 +18,7 @@ public class Game extends JGEngine
 	public Enemy enemy = null;
 	private Levels objLevels;
 	public String passString;
-	public boolean debug = true;
+	public boolean debug = false;
 	public int tileWidth = 40;
 	public int tileHeight = 40;
 	private int xofs, yofs = 0;
